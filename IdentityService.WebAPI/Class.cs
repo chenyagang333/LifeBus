a0b1dd5b5aee567adc2b71bb88408f8c273fc297
chenyagang <13050997+chen-yagang@user.noreply.gitee.com>
2024-06-25 17:18:39 +0800

    添加项目文件。

---
+++ IdentityService.WebAPI/Class.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace IdentityService.WebAPI
+{
+    public class Class : IAsyncActionFilter
+    {
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
